0277b7ee120a50914cd7e17b4e577aad57b97569
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-07-16 18:15:35 +0600

    created the UI and added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["corporate"],
+  },
+  plugins: [require("daisyui")],
+}
